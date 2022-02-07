3933d7d680bac6cfdca85976f7229621734f106f
mhgamble1 <mhgamble1@gmail.com>
2022-02-06 21:00:45 -0600

    changed table headers to buttons

--- client/src/components/wordTable.jsx
+++ client/src/components/wordTable.jsx
@@ -1,16 +1,30 @@
+import React from "react";
 export default function WordTable(props) {
 	const { words } = props;
+	const [sortedField, setSortedField] = React.useState(null);
 	return (
 		<div>
 			<h3>Word List</h3>
 			<table>
 				<thead>
 					<tr>
-						<th>Text</th>
-						<th>Title</th>
-						<th>Author</th>
-						<th>Date</th>
-						<th>Time</th>
+						<th>
+							<button type="button" onClick={() => setSortedField('Text')}>
+								Text
+							</button>
+						</th>
+						<th><button type="button" onClick={() => setSortedField('Title')}>
+							Title
+						</button></th>
+						<th><button type="button" onClick={() => setSortedField('Author')}>
+							Author
+						</button></th>
+						<th><button type="button" onClick={() => setSortedField('Date')}>
+							Date
+						</button></th>
+						<th><button type="button" onClick={() => setSortedField('Time')}>
+							Time
+						</button></th>
 					</tr>
 				</thead>
 				<tbody>
