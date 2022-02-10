3f055ae00cca3fe2225fe9c72929b85a0ff52d54
mhgamble1 <mhgamble1@gmail.com>
2022-02-10 10:51:31 -0600

    added dictionaryapi request on fullpageword load

--- client/src/components/fullPageWord.jsx
+++ client/src/components/fullPageWord.jsx
@@ -1,45 +1,5 @@
-// import * as React from 'react'
-// import axios from 'axios';
-// import { useParams } from 'react-router-dom'
-
-// export default function FullPageWord() {
-// 	// const [word, setWord] = React.useState(null);
-// 	// const {Text} = useParams();
-
-// 	// React.useEffect(() => {
-// 		// setWord(Text);
-// 	// })
-
-// 	// return (
-// 	// 	<h1>{word}</h1>
-// 	// )
-// 	const { _id } = useParams();
-// 	// const [ID, setID] = React.useState();
-// 	const [data, setData] = React.useState({words: []});
-
-// 	React.useEffect(() => {
-// 		// setID(_id);
-// 		axios
-// 			.get(`http://localhost:5000/word/${_id}`)
-// 			.then((response) => {
-// 				console.log(response.data)
-// 				setData(response.data);
-// 			})
-// 			.catch(function (error) {
-// 				console.log(error);
-// 			});
-// 	}, []);
-
-// 	if (data == null) {
-// 		return null;
-// 	}
-
-// 	return (
-// 		<h1>{_id}</h1>
-// 	)
-// }
-
 import * as React from 'react'
+import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 import { useParams } from 'react-router-dom'
 
@@ -52,17 +12,17 @@ export default function FullPageWord() {
 
 	React.useEffect(() => {
 		setID(_id);
-	});
-	// 	axios
-	// 		.get(`http://localhost:5000/word/${_id}`)
-	// 		.then((response) => {
-	// 			console.log(response.data)
-	// 			setData(response.data);
-	// 		})
-	// 		.catch(function (error) {
-	// 			console.log(error);
-	// 		});
-	// }, []);
+		axios
+			.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${currentword.Text}`)
+			.then((response) => {
+				console.log(response.data)
+				setData(response.data);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}, []);
+
 
 	return (
 		<div>
