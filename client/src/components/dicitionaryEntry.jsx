3baaae04e106a0510521f829af314f498c75cdd2
mhgamble1 <mhgamble1@gmail.com>
2022-02-13 15:19:31 -0600

    using ListGroup in dictionaryEntry component

--- client/src/components/dicitionaryEntry.jsx
+++ client/src/components/dicitionaryEntry.jsx
@@ -1,75 +1,68 @@
 import Player from '../utils/useAudio';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
 
 export default function DictionaryEntry(props) {
-	// if (props == null) {
-	// 	return null;
-	// }
-
 	let data = props.data[0];
+	let currentword = props.currentword;
 
 	// if data contains phonetics fields
 	let { word, phonetic, phonetics, origin, meanings } = data;
 
 	return (
-		<>
+		<ListGroup variant="flush">
 			{/* let's return the word and just hide the word from the passed location data */}
-			<p>{data.word}</p>
-
+			<ListGroup.Item>{word}</ListGroup.Item>
 			{/* return phonetic for now */}
-			<p>{data.phonetic}</p>
+			{/* <p>{phonetic}</p> */}
 
 			{/* map over phonetics */}
 
-			<p>phonetics: </p>
-			<ol>
-				{phonetics.map((element, index) => {
-					return (
-						<li key={index}>
-							{/* <p>{element.audio}</p> */}
-							<Player url={element.audio} word={data.word}/>
-							<p>{element.text}</p>
-						</li>
-					)
-				})}
-			</ol>
+			<ListGroup.Item>
+				<Player url={phonetics[0].audio} word={phonetics[0].text}>
+				</Player>
+			</ListGroup.Item>
 
 			{/* display origin */}
-			<p>origin: {origin}</p>
+			{origin != null && <ListGroup.Item>origin: {origin}</ListGroup.Item>}
 
 			{/* map over meanings*/}
-			<p>meanings:</p>
-			<ol>
+			{/* <ListGroup.Item>meanings:</ListGroup.Item> */}
+			<ListGroup.Item>
 				{meanings.map((element, index) => {
 					return (
-						<li key={index}>
-							<p>{element["partOfSpeech"]}</p>
+						<ListGroup key={index} variant="flush">
+							<ListGroup.Item><em>part of speech:</em> {element["partOfSpeech"]}</ListGroup.Item>
 							{/* map over definitions */}
-							<ol>
+							<ListGroup as="ol" numbered variant="flush">
 								{element["definitions"].map((element, index) => {
 									return (
-										<li key={index}>
-											<p>{element["definition"]}</p>
-											<p>example: {element["example"]}</p>
-											{/* map over synonyms */}
-											<ul>
-												{element["synonyms"].map((element, index) => {
-													<li key={index}>{element}</li>
-												})}
-											</ul>
-											{/* map over antonyms */}
-											<ul>
-												{element["antonyms"].map((element, index) => {
-													<li key={index}>{element}</li>
-												})}
-											</ul>
-										</li>
+										<ListGroup.Item as="li">
+											<ListGroup key={index} variant="flush">
+												<ListGroup.Item><em>definition:</em> {element["definition"]}</ListGroup.Item>
+												<ListGroup.Item><em>example:</em> {element["example"]}</ListGroup.Item>
+												{/* map over synonyms */}
+												<ListGroup variant="flush">
+													{element["synonyms"].map((element, index) => {
+														<ListGroup.Item key={index}>{element}</ListGroup.Item>
+													})}
+												</ListGroup>
+												{/* map over antonyms */}
+												<ListGroup variant="flush">
+													{element["antonyms"].map((element, index) => {
+														<ListGroup.Item key={index}>{element}</ListGroup.Item>
+													})}
+												</ListGroup>
+											</ListGroup>
+										</ListGroup.Item>
 									)
 								})}
-							</ol>
-						</li>
+							</ListGroup>
+						</ListGroup>
 					)
 				})}
-			</ol>
-		</>
+			</ListGroup.Item>
+			<ListGroup.Item>Added to my words on {currentword.Date} at {currentword.Time}</ListGroup.Item>
+			<ListGroup.Item>From "{currentword.Title}"" by {currentword.Author}</ListGroup.Item>
+		</ListGroup>
 	)
 };
