c38841270bc8f19b87e350848b33e3f75dbed4c3
mhgamble1 <mhgamble1@gmail.com>
2022-02-15 14:00:57 -0600

    added a fourth chart

--- client/src/components/chartDashboard.jsx
+++ client/src/components/chartDashboard.jsx
@@ -7,6 +7,7 @@ export default function ChartDashboard() {
 			<Chart height={"100vh"} width={"100vw"} chartId={'4c48ab30-300e-4d2a-8754-021b2a4211ea'} />
 			<Chart height={"100vh"} width={"100vw"} chartId={'9c14967a-1b0c-4043-9304-282b642e0a14'} />
 			<Chart height={"100vh"} width={"100vw"} chartId={'1efd63bb-d116-4b76-a959-8744946857e0'} />
+			<Chart height={"100vh"} width={"100vw"} chartId={'a55feb89-7bbb-48c1-888f-0dfbefa2d50b'} />
 		</div>
 	)
 }
