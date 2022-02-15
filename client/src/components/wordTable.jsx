743798e7d8ed7dbc5644f81b255fa16f305b0c4c
mhgamble1 <mhgamble1@gmail.com>
2022-02-15 14:39:04 -0600

    added prompt to click on words

--- client/src/components/wordTable.jsx
+++ client/src/components/wordTable.jsx
@@ -36,7 +36,7 @@ export default function WordTable(props) {
 
 	return (
 		<div>
-			<h3>Word List</h3>
+			<h1>select a word for dictionary entry</h1>
 			<Table striped bordered hover>
 				<thead>
 					<tr>
