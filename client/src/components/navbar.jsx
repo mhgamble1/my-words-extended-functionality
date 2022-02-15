44f55a6392f08597e2adeb1f460cd2c119d69462
mhgamble1 <mhgamble1@gmail.com>
2022-02-15 14:39:17 -0600

    added button to navlink

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,20 +1,27 @@
 import React from "react";
- 
+
 // We import bootstrap to make our application look better.
 import "bootstrap/dist/css/bootstrap.css";
- 
+
 // We import NavLink to utilize the react router.
 import { NavLink } from "react-router-dom";
- 
+import { Button } from "react-bootstrap";
+
 // Here, we display our Navbar
 const Navbar = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <NavLink className="navbar-brand" to="/">
-          My Words
+          <Button variant="outline-primary">
+            My Words
+          </Button>
         </NavLink>
-        <NavLink className="navbar-brand" to="/chartdashboard">Charts</NavLink>
+        <NavLink className="navbar-brand" to="/chartdashboard">
+          <Button variant="outline-primary">
+            Charts
+          </Button>
+        </NavLink>
         {/* <button
           className="navbar-toggler"
           type="button"
@@ -30,5 +37,5 @@ const Navbar = () => {
     </div>
   );
 };
- 
+
 export default Navbar;
