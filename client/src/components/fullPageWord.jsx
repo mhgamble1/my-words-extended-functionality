769c66b5ab9d279afae88ce79bb65b789f11c87f
mhgamble1 <mhgamble1@gmail.com>
2022-02-18 19:16:46 -0600

    handle 404 in fullPageWord

--- client/src/components/fullPageWord.jsx
+++ client/src/components/fullPageWord.jsx
@@ -3,29 +3,38 @@ import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 import { useParams } from 'react-router-dom'
 import DictionaryEntry from './dicitionaryEntry';
+import NotFound from './notFound';
 
 export default function FullPageWord() {
 	const [data, setData] = React.useState(null);
+	const [notFound, setNotFound] = React.useState(false);
 	const { _id } = useParams();
 	const location = useLocation();
 	const { currentword } = location.state;
 
 	React.useEffect(() => {
-		axios // still need to deal with a "not found" response
+		axios
 			.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${currentword.Text}`)
 			.then((response) => {
 				setData(response.data);
 			})
 			.catch(function (error) {
+				if (error.response.status === 404) {
+					setNotFound(true);
+				}
 				console.log(error);
 			});
 	}, []);
 
 
+	if (notFound) {
+		return <NotFound />
+	}
+
 	return (
 		<div>
 			{data != null &&
-				<DictionaryEntry data={data} currentword={currentword}/>
+				<DictionaryEntry data={data} currentword={currentword} />
 			}
 		</div>
 	)
